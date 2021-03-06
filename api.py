6fef354088e4e0c50137fb6f79f0454e5bcaa0d0
Robert Hanacek <robert@idea-loop.com>
2021-03-06 10:41:23 -0700

    Generalize API for REST.

--- api.py
+++ api.py
@@ -13,28 +13,27 @@ questions = [
 def ask(q):
     print(q['text'])
 
-    all_answers = {i:(k,v) for i, (k,v) in enumerate(q['answers'].items(), start=1)}
-    correct_answers = [a for (a, correct) in all_answers.values() if correct]
+    numbered_answers = {i:k for i, k in enumerate(q['answers'].keys(), start=1)}
 
-    for k, v in all_answers.items():
-        answer, _ = v
-        print('%d: %s' % (k, answer))
+    for i, answer in numbered_answers.items():
+        print('%d: %s' % (i, answer))
 
-    return all_answers, correct_answers
+    return numbered_answers
 
-def correct(answers):
-    return [a for (a, correct) in answers.values() if correct]
+def correct_answers_to(question):
+    return [a for (a, correct) in question['answers'].items() if correct]
+
+def check(answer, question):
+    return answer in correct_answers_to(question)
 
 def main():
     for q in questions:
-        all_answers, correct_answers = ask(q)
-        a = int(input())
-        answered, _ = all_answers[a]
-        print('Answered:', answered)
-        print('Correct:', correct_answers)
-        print('Correct!' if answered in correct_answers else 'Wrong...')
-        
+        numbered_answers = ask(q)
+        i = int(input())
+        a = numbered_answers[i]
 
+        print('%s is %s' % (a, 'correct' if check(a, q) else 'wrong'))
+        print('Correct answer:', ' '.join(correct_answers_to(q)))
 
 if __name__ == "__main__":
     main()
