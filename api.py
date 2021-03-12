a6141f76b0b47d710dcf52e8b0c7de8880cdbfec
Robert Hanacek <robert@idea-loop.com>
2021-03-12 11:50:31 -0700

    Add tests. Closes #2

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import random
+import unittest
 
 questions = [
     {
@@ -67,11 +68,11 @@ def ask(q):
 
     return numbered_answers
 
-def correct_answers_to(question):
-    return [a for (a, correct) in question['answers'].items() if correct]
-
 def check(answers, question):
     return set(answers) == set(correct_answers_to(question))
+
+def correct_answers_to(question):
+    return [a for (a, correct) in question['answers'].items() if correct]
 
 def main():
 
@@ -86,5 +87,42 @@ def main():
         print('correct' if check(answers, q) else 'wrong')
         print('Correct answer(s):', ' '.join(correct_answers_to(q)))
 
+class CitizenshipTest(unittest.TestCase):
+
+    q = {
+        'text': 'Question with 2 correct options',
+        'answers': {
+             "France": False,
+             "England": True,
+             "Spain": False, 
+             "Russia": True,
+        },
+    }
+    
+    def test_check_complete_answer(self):
+        answers = ['England', 'Russia']
+        self.assertTrue(check(answers, self.q))
+
+    def test_check_complete_answer_different_order(self):
+        answers = ['Russia', 'England']
+        self.assertTrue(check(answers, self.q))
+
+    def test_check_incomplete_answer(self):
+        answers = ['England']
+        self.assertFalse(check(answers, self.q))
+
+    def test_check_wrong_answer(self):
+        answers = ['Spain']
+        self.assertFalse(check(answers, self.q))
+
+    def test_check_invalid_answer(self):
+        answers = ['Foo']
+        self.assertFalse(check(answers, self.q))
+
+    def test_check_empty_answer(self):
+        answers = []
+        self.assertFalse(check(answers, self.q))
+
 if __name__ == "__main__":
-    main()
+    # main()
+    unittest.main()
